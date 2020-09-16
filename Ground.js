3c8e3dd0dab25e74bf60ac2e1b5bd312bf31c46e
arya7-max <68646383+arya7-max@users.noreply.github.com>
2020-09-16 17:26:07 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,18 @@
+class Ground {
+    constructor() {
+      var options = {
+          isStatic: true
+      }
+      this.ground = Bodies.rectangle(450,390,900,20,options);
+      
+      World.add(world, this.ground);
+    }
+    display(){
+     noStroke();
+
+
+      rectMode(CENTER);
+      fill("brown");
+      rect(this.ground.position.x,this.ground.position.y,920);
+    }
+  };
